57559342cd8dcd9d723c5ffc8d707a645361a0a5
Guillermo García <55685909+ggarredondo@users.noreply.github.com>
2021-11-24 14:54:44 +0000

    attack animation

--- Assets/Scripts/Combat/EnemyCombat.cs
+++ Assets/Scripts/Combat/EnemyCombat.cs
@@ -10,12 +10,13 @@ public class EnemyCombat : MonoBehaviour
     public GameObject UI;
     public GameObject enemy_object;
     public GameObject player_object;
-    public float depletion_transition_time = 2f, depletion_transition_speed = 2f;
+    public float depletion_transition_speed = 2.5f, depletion_transition_threshold = 0.005f;
 
     private Image health_bar;
     private PlayerCombat player;
     private float timer;
     private Vector3 target_health_scale;
+    private GameObject cut_animation;
 
     // Start is called before the first frame update
     void Start() {
@@ -23,17 +24,17 @@ public class EnemyCombat : MonoBehaviour
         UI.transform.Find("CombatUI").Find("EnemySprite").GetComponent<Image>().sprite = sprite_renderer.sprite;
         health_bar = UI.transform.Find("CombatUI").Find("EnemyHealthBar").Find("ProgressForeground").GetComponent<Image>();
         player = player_object.GetComponent<PlayerCombat>();
+        cut_animation = UI.transform.Find("CombatUI").Find("Cut").gameObject;
     }
 
     public void TakeDamage(float damage)
     {
-        timer = Time.time + depletion_transition_time;
         stats.health -= damage;
         target_health_scale = new Vector3(stats.health / stats.max_health, 1f, 1f);
-        if (stats.health <= 0) {
-            player.current_state = TurnState.WAITING;
-            enemy_object.SetActive(false);
-        }
+        if (stats.health <= 0) { //temp
+            player.current_state = TurnState.WAITING; //temp
+            enemy_object.SetActive(false); //temp
+        } //temp
     }
 
     float interval = 4f;//temp
@@ -41,6 +42,7 @@ public class EnemyCombat : MonoBehaviour
     void Update()
     {
         UI.transform.Find("CombatUI").Find("EnemyHealthBar").gameObject.SetActive(player.current_state == TurnState.ATTACKING);
+        cut_animation.SetActive(player.current_state == TurnState.ATTACKING);
 
         switch (player.current_state)
         {
@@ -50,13 +52,17 @@ public class EnemyCombat : MonoBehaviour
                 break;
 
             case TurnState.ATTACKING:
-                health_bar.transform.localScale = Vector3.Lerp(health_bar.transform.localScale, target_health_scale,
-                    Time.deltaTime * depletion_transition_speed);
+                if (cut_animation.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("End")) {
+                    cut_animation.GetComponent<Image>().color = new Color(1f, 1f, 1f, 0f);
+                    health_bar.transform.localScale = Vector3.Lerp(health_bar.transform.localScale, target_health_scale,
+                        Time.deltaTime * depletion_transition_speed);
+                }
 
-                if (timer <= Time.time) {
+                if (Vector3.Distance(health_bar.transform.localScale, target_health_scale) <= depletion_transition_threshold) {
                     health_bar.transform.localScale = target_health_scale;
                     player.current_state = TurnState.TRANSITION_TO_AVOID;
-                    timer = Time.time + interval;
+                    cut_animation.GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
+                    timer = Time.time + interval; //temp
                 }
                 break;
         }
